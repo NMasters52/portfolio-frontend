eca3fc121750b8e1a1e060ec924d5f877f7afa80
nmasters <nmasters52@gmail.com>
2025-09-08 11:46:30 -0400

    feat(types) add type for Project array. Impliment on project loader return and project destructuring.

---
+++ app/types.ts
@@ -0,0 +1,10 @@
+export type Project = {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    url: string;
+    category: string;
+    date: string;
+    featured: boolean;
+}
