2f7a0ad6e024e71ddbd52fef1714cd13a35e49f6
nmasters <nmasters52@gmail.com>
2025-09-13 05:27:02 -0400

    add(pagination and markdown)markdown styled and pagination working
    - Installed @tailwindcss/typography as a plugin. Markdown can now be styled with the plugins classes
    - Added pagination logic and component for the posts index.tsx

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 //types
 import type { Route } from "./+types"
 import type { PostMeta } from "~/types";
 //components
 import PostCard from "~/components/PostCard";
+import Pagination from "~/components/Pagination";
 
 
 export async function loader({request}: Route.LoaderArgs):Promise<{posts: PostMeta[]}> {
@@ -12,20 +14,38 @@ export async function loader({request}: Route.LoaderArgs):Promise<{posts: PostMe
   if (!res.ok) throw new Error('Failed to fetch data');
 
   const data = await res.json();
+
+  data.sort((a: PostMeta,b: PostMeta) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime(); 
+  })
+
   return {posts: data};
 }
 
 const BlogPage = ({loaderData}: Route.ComponentProps) => {
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
   const {posts} = loaderData;
-  console.log(posts);
+
+  const totalPages = Math.ceil(posts.length/postsPerPage);
+  const lastIndex = postsPerPage * currentPage;
+  const firstIndex = lastIndex - postsPerPage;
+
+  const currentPosts = posts.slice(firstIndex, lastIndex);
 
   return (
     <div className='mx-auto max-w-3xl mt-10 px-6 py-6 bg-gray-900'>
         <h2 className='text-3xl text-white font-bold mb-8'>📝 Blog</h2>
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
          <PostCard key={post.slug} post={post} />
         ))}
+
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
     </div>
   )
 }
