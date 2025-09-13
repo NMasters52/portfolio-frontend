cd5785ca58b6b6d05887392824696d9165d16103
nmasters <nmasters52@gmail.com>
2025-09-13 04:47:59 -0400

    feat(post details) add markdown and styling for each post

--- app/routes/blog/details.tsx
+++ app/routes/blog/details.tsx
@@ -1,7 +1,7 @@
-import { mark } from "framer-motion/client";
 import type { Route } from "./+types/details"
+import type { PostMeta } from "~/types"
 import ReactMarkdown from 'react-markdown'
-import { PostMeta } from "~/types"
+import { Link } from "react-router";
 
 export async function loader({request, params}: Route.LoaderArgs) {
     const { slug } = params;
@@ -16,7 +16,7 @@ export async function loader({request, params}: Route.LoaderArgs) {
     const postMeta = index.find((post:PostMeta) => post.slug === slug);
     if (!postMeta) throw new Response('Not Found', {status: 404});
 
-    // dynamicall import raw markdow
+    // dynamically import raw markdow
     const markdown = await import(`../../posts/${slug}.md?raw`);
 
     return {
@@ -36,10 +36,24 @@ const BlogPostDetailsPage = ({loaderData}: BlogPostDetailsPageProps) => {
 
     const {postMeta, markdown} = loaderData;
 
-    console.log(postMeta, markdown)
-
   return (
-    <div>details</div>
+    <div className='max-w-3xl mx-auto px-6 py-12 bg-gray-900'>
+      <h1 className='text-3xl font-bold text-blue-400 mb-2'>{postMeta.title}</h1>
+      <p className='text sm text-gray-400 mb-6'>
+        {new Date(postMeta.date).toDateString()}
+      </p>
+
+      <div className='prose prose-invert max-w-none mb-12'>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+
+      <Link
+        to='/blog'
+        className='inline-block bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition'
+      >
+        ← Back To Posts
+      </Link>
+    </div>
   )
 }
 
