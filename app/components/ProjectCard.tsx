bfada9b26d8bc16f018828c454f5eea003faa013
nmasters <nmasters52@gmail.com>
2025-09-20 06:21:31 -0400

    adjust params id to reflect strapis data structure to return the documentId instead of id

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,23 +1,29 @@
-import { Link } from "react-router"
-import type { Project } from "~/types"
+import { Link } from "react-router";
+import type { Project } from "~/types";
 
-const ProjectCard = ( {project}: {project: Project}  ) => {
+const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <Link to={`/projects/${project.id}`} className="block transform transition duration-300 hover:scale-[1.02]">
-        <div className="bg-gray-800 border-gray-700 rounded-lg overflow-hidden shadow-sm hover:shadow-md transition">
-            <img src={project.image} alt={project.title} className="w-full h-40 object-cover"/>
-            <div className="p-5">
-                <h3 className="font-semibold text-blue-400 mb-1">{project.title}</h3>
-                <p className="text-sm text-gray-300 mb-2">{project.description}</p>
-                <div className="flex justify between items-center text-sm text-gray-400">
-                    <span>{project.category}</span>
-                    <span>{new Date(project.date).toLocaleDateString()}</span>
-                </div>
-            </div>
+    <Link
+      to={`/projects/${project.documentId}`}
+      className="block transform transition duration-300 hover:scale-[1.02]"
+    >
+      <div className="bg-gray-800 border-gray-700 rounded-lg overflow-hidden shadow-sm hover:shadow-md transition">
+        <img
+          src={project.image}
+          alt={project.title}
+          className="w-full h-40 object-cover"
+        />
+        <div className="p-5">
+          <h3 className="font-semibold text-blue-400 mb-1">{project.title}</h3>
+          <p className="text-sm text-gray-300 mb-2">{project.description}</p>
+          <div className="flex justify between items-center text-sm text-gray-400">
+            <span>{project.category}</span>
+            <span>{new Date(project.date).toLocaleDateString()}</span>
+          </div>
         </div>
+      </div>
     </Link>
-    
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -1,35 +1,56 @@
-import type { Route } from "./+types"
-import type { Project } from "~/types"
-import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router";
+//types
+import type { Route } from "./+types";
+import type { Project, StrapiProject, StrapiResponse } from "~/types";
+//react icons
+import { FaArrowLeft } from "react-icons/fa";
 
-export async function clientLoader({ request, params }: Route.ClientLoaderArgs): Promise<Project> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects/${params.id}`);
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const { id } = params;
 
-  if (!res.ok) throw new Response('Project not found', {status: 404});
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/projects?filters[documentId][$eq]=${id}&populate=*`
+  );
 
-  const project: Project = await res.json();
-  return project;
+  if (!res.ok) throw new Response("Project not found", { status: 404 });
+
+  const json: StrapiResponse<StrapiProject> = await res.json();
+
+  const item = json.data[0];
+
+  const project: Project = {
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url
+      ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
+      : "/images/no-image.png",
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured,
+  };
+
+  return { project };
 }
 
-export function HydrateFallback() {
-  return <div>Loading...</div>;
-}
-
-const details = ( {loaderData}: Route.ComponentProps ) => {
-  const project = loaderData;
-  
+const details = ({ loaderData }: Route.ComponentProps) => {
+  const { project } = loaderData;
 
   return (
     <>
-      <Link to="/projects" className="flex items-center text-blue-500 mb-6 transition"> 
+      <Link
+        to="/projects"
+        className="flex items-center text-blue-500 mb-6 transition"
+      >
         <FaArrowLeft className="mr-2" /> Back to Projects
       </Link>
       <div className="grid gap-8 md:grid-cols-2 items-start">
         <div>
-          <img 
-            src={project.image} 
-            alt={project.title} 
+          <img
+            src={project.image}
+            alt={project.title}
             className="w-full rounded-lg shadow-md"
           />
         </div>
@@ -40,17 +61,19 @@ const details = ( {loaderData}: Route.ComponentProps ) => {
           <p className="text-gray-300 text-sm mb-4">
             {new Date(project.date).toLocaleDateString()} | {project.category}
           </p>
-          <p className="text-200 mb-6">
-            {project.description}
-          </p>
+          <p className="text-200 mb-6">{project.description}</p>
 
-          <a href={project.url} target="_blank" className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded transition">View Live Site</a>
+          <a
+            href={project.url}
+            target="_blank"
+            className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded transition"
+          >
+            View Live Site
+          </a>
         </div>
-
       </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default details
+export default details;
